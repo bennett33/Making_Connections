546a4e3f1a8aab1d9d2d95a4e5a8dd89d453a314
bennett33 <blanzetta3@gmail.com>
2021-12-21 17:16:09 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var janeName = document.querySelector("#jane-name");
+
+function editName() {
+    janeName.innerText = "Keanu Reeves" 
+}
+
+
+
+//-------//
+
+var toddReq = document.querySelector("#todd");
+
+function removeTodd() {
+    toddReq.remove();
+}
+    
+
+
+//----------//
+
+var philReq = document.querySelector("#phil");
+
+function removePhil() {
+    philReq.remove();
+}
+    
+
+
